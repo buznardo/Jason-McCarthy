0e2a407b77d8048590b998952200db51f1f20279
Colm Tuite <colmtuite@gmail.com>
2012-03-08 16:22:26 +0000

    Initial Commit

---
+++ app/assets/javascripts/script.js
@@ -0,0 +1,8 @@
+//$('header a').hover(
+  //  function(){
+    //     $(this).css({color:'pink'}); //mouseover
+    //},
+   // function(){
+    //     $(this).css({color:'#aaa'}); // mouseout
+   // }
+//);
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jason::Application.config.secret_token = '4941d4d0eb157f16b1638dc8fad4968d9c4b71347e33dd2f628e3aafc51392895e599e110bbbe7b6b79d2b2535fbcf3cbd72f3e32e24c1066875328cf533c862'
