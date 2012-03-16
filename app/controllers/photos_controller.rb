919fbfd0ef13a10c6a9dc06446f788e2165d9952
Colm Tuite <colmtuite@gmail.com>
2012-03-16 10:57:23 +0000

    Loads of issues about the place

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -4,4 +4,14 @@ class PhotosController < ApplicationController
     @photo = @shoot.photos.create(params[:photo])
     redirect_to shoot_path(@shoot)
   end
+  
+  def destroy
+    @photo = Photo.find(params[:id])
+    @photo.destroy
+    
+    respond_to do |format|
+      format.html { redirect_to shoots_url }
+      format.json { head :no_content }
+    end
+  end
 end
