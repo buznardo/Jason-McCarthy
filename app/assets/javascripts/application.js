27b618d96f8b17e3d7ba69ead5c21912198cd43e
David Tuite <dtuite@gmail.com>
2012-04-30 10:09:31 +0100

    Fix Bug: JS execution before images loaded

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,26 +1,18 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// the compiled file.
-//
-// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
-// GO AFTER THE REQUIRES BELOW.
 //
 //= require jquery
 //= require jquery_ujs
 //= require lemmon-slider.js
-//= require_tree .
+// require_tree .
 
 
-$(function() {
-  $('#slider').lemmonSlider({
-  });
+// NOTE: window.load() != $(document).ready()
+// INFO: http://stackoverflow.com/a/545005/574190
+$(window).load(function() {
+  $('#slider').lemmonSlider({});
 });
 
-$('.hello').bind("ajax:before", function() {
-  $('ul.thumbs').html("loading");
+$(function() {
+  $('.hello').bind("ajax:before", function() {
+    $('ul.thumbs').html("loading");
+  });
 });
