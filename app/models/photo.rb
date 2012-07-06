a7a8a2d139343ba6b5880b26f507a9a29f344eaf
Colm Tuite <colmtuite@gmail.com>
2012-07-06 10:58:12 +0100

    Updated logo

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -6,7 +6,7 @@ class Photo < ActiveRecord::Base
   }.merge(S3_SETTINGS)
 
   validates_attachment_presence :pic
-  validates_attachment_size :pic, :less_than => 0.1.megabytes
+  validates_attachment_size :pic, :less_than => 0.2.megabytes
   validates_attachment_content_type :pic, :content_type => ['image/jpeg', 'image/png']
   
   belongs_to :shoot
